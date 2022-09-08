26fcf40e24db5039dcc4627bb1b347b850e4befa
Mpqg <4534@holbertonschool.com>
2022-09-07 22:56:32 -0500

    script that prepares a MySQL server

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+--Creates a MySQL database for the application:
+-- 1. Creates a database hbnb_dev_db
+-- 2. Creates a user hbnb_dev with password hbnb_dev_pwd in localhost
+-- 3. Grants all privileges on hbnb_dev_db to hbnb_dev
+-- 4. Grants SELECT privilege for hbnb_dev on performance_schema.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 
+	IF NOT EXISTS 'hbnb_dev'@'localhost' 
+	IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+FLUSH PRIVILEGES;
