7c6b403574d0a41879eeb87cfc7946c2c62637e5
Mpqg <4534@holbertonschool.com>
2022-09-07 23:05:31 -0500

    FIXING CODE

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,11 +3,13 @@
 -- 2. Creates a user hbnb_test with password hbnb_test_pwd in localhost
 -- 3. Grants all privileges on hbnb_test to hbnb_test_db
 -- 4. Grants SELECT privilege for hbnb_test on performance_schema.
+-- User hbnb_test has all permissions on hbnb_test_db
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER 
 	IF NOT EXISTS 'hbnb_test'@'localhost' 
 	IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES 
+	ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
