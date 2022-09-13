dd45a3667d38790b28c07a7171df466ccb273862
Mpqg <mapazquiga@gmail.com>
2022-09-13 16:45:55 -0500

    Fixing code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,12 +9,12 @@ Script starts Flask web app
             /states_list: Display HTML and state info from storage;
 """
 
+from email.policy import strict
 from models import storage
 from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -23,13 +23,13 @@ def teardown_db(self):
     storage.close()
 
 
-@app.route('/states_list')
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
        States are sorted by name.
     """
-    states = storage.all(State).values()
-    return (render_template("7-states_list.html", states=states))
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
 
 
 if __name__ == "__main__":
