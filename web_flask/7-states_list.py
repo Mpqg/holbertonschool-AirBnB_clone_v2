7864d3cc873788dd1f103cfc87493627cf81f52e
Mpqg <mapazquiga@gmail.com>
2022-09-16 11:23:14 -0500

    Deleting commetaries

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
 Script starts Flask web app
-    listen on 0.0.0.0, port 5000
-    routes: /:
-            /states_list: Display HTML and state info from storage;
 """
 
 from models import storage
@@ -14,15 +11,11 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_db(self):
-    """After each request remove current SQLAlchemy session"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-       States are sorted by name.
-    """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
