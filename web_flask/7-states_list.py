42cf37b016bb32bfea3ec84191cf9c57793a1d0b
Mpqg <mapazquiga@gmail.com>
2022-09-16 11:53:18 -0500

    Fixing code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""This module starts a Flask web application and displays an integer in HTML
-"""
-from flask import Flask
-from flask import escape
-from flask import render_template
+""" Test """
+
 from models import storage
-from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states():
-    """displays states"""
-    state_dict = storage.all(State)
-    return render_template('7-states_list.html', state_dict=state_dict)
-
-
 @app.teardown_appcontext
 def teardown_db(self):
-    """removes the current SQLAlchemy Session"""
+    """Test"""
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Test"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
